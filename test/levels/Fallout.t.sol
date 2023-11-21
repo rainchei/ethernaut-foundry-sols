66b5c73a31c346756357b68c6ab2eb6e522ee563
rainchei <rainchei.lin@gmail.com>
2023-11-21 14:51:00 +0800

    Add levels/Fallout

---
+++ test/levels/Fallout.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {SetUpLevelTest} from "../SetUpLevelTest.sol";
+import {Fallout, FalloutFactory} from "../helpers/FalloutFactory.sol";
+
+contract FalloutLevel is SetUpLevelTest {
+    Fallout internal lv;
+
+    function setUp() public override {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        level = address(new FalloutFactory());
+        super.setUp();
+        lv = Fallout(payable(instance));
+    }
+
+    function exploitLevel() internal override {
+        /** CODE YOUR SOLUTION HERE */
+    }
+}
