efe9b89ae0ab7ce7f224b9778b689dd708b580df
rainchei <rainchei.lin@gmail.com>
2023-11-22 12:03:27 +0800

    add levels/Delegation

---
+++ test/levels/Delegation.t.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {SetUpLevelTest} from "../SetUpLevelTest.sol";
+import {Delegation, DelegationFactory} from "ethernaut/levels/DelegationFactory.sol";
+
+contract DelegationLevel is SetUpLevelTest {
+    Delegation internal lv;
+
+    function setUp() public override {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        level = address(new DelegationFactory());
+        super.setUp();
+        lv = Delegation(instance);
+    }
+
+    function exploitLevel() internal override {
+        /** CODE YOUR SOLUTION HERE */
+    }
+}
