9ec48a70a78d7a76c291958b6417c5d202eab5b1
rainchei <rainchei.lin@gmail.com>
2023-11-30 11:23:31 +0800

    add levels/Motorbike

---
+++ test/levels/Motorbike.t.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import {SetUpLevelTest} from "../SetUpLevelTest.sol";
+import {Motorbike, MotorbikeFactory} from "../helpers/MotorbikeFactory.sol";
+
+contract MotorbikeLevel is SetUpLevelTest {
+    Motorbike internal motorbike;
+
+    function setUp() public override {
+        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+        instanceBalance = 0 ether;
+        playerBalance = 0 ether;
+        level = address(new MotorbikeFactory());
+        super.setUp();
+        motorbike = Motorbike(payable(instance));
+    }
+
+    function exploitLevel() internal override {
+        /** CODE YOUR SOLUTION HERE */
+    }
+}
